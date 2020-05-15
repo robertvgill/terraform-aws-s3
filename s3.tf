512cfdea208a154b89ed38e7da4114a0c7c6b35a
Robert Gill <robertvgill@gmail.com>
2020-05-15 14:53:30 +0800

    .

--- s3.tf
+++ s3.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket" "terraform_state_s3" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-lock-dynamo"
+  name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
--- variables.tf
+++ variables.tf
@@ -22,3 +22,8 @@ variable "bucket_name" {
   description = "The name of the S3 bucket"
   type = string
 }
+
+variable "dynamodb_table_name" {
+  description = "The name of the DynamoDB table"
+  type = string
+}
