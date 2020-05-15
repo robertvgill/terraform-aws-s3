90770d2738eaeceeced4da6e96456cbfb56b817e
Robert Gill <robertvgill@gmail.com>
2020-05-15 19:22:31 +0800

    .

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,6 @@ output "s3_bucket_region" {
 }
 
 output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.terraform_locks.name
-  description = "The name of the DynamoDB table"
+  value       = element(concat(aws_dynamodb_table.terraform_locks.*.name, list("")), 0)
+  description = "The name of the DynamoDB table."
 }
